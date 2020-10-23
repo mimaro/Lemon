51bef8072a767231de4e4be5fb253432f6d23447
mimaro <46071857+mimaro@users.noreply.github.com>
2020-10-24 00:58:24 +0200

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,6 +2,6 @@ import pytesseract
 #pytesseract.pytesseract.tesseract_cmd = r'/home/pi/.local/lib/python3.7/site-packages/pytesseract'
 #pytesseract.pytesseract.tesseract_cmd = r'/home/pi/.local/bin/pytesseract'
 from PIL import Image
-img =Image.open ('Beispiel2.jpg')
+img =Image.open ('Bild.png')
 text = pytesseract.image_to_string(img, config='')
 print (text)
